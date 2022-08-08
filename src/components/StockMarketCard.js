3772236b55183f977e93074a3f92846c6f88bddd
alias-3 <wanianurag3@gmail.com>
2022-08-08 23:16:56 +0530

    stock market card

--- src/components/StockMarketCard.js
+++ src/components/StockMarketCard.js
@@ -4,40 +4,56 @@ import { Card } from 'react-bootstrap'
 
 class StockMarketCard extends Component {   
 
-    constructor(props) {
-        super(props);        
-    }
+    // constructor(props) {
+    //     super(props);        
+    // }
     
-    componentDidMount() {
-        this.renderWidget();
-    }
+    // componentDidMount() {
+    //     // this.renderWidget();
+    //     //this.resizeAllIFramesToFitContent();
+    // }
 
-    renderWidget() {
-        if (document.getElementsByClassName("tradingview-widget-container")) {
-            const script = document.createElement('script');
-            script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js'
-            script.async = true;
-            script.innerHTML = JSON.stringify({
-                "symbol": "FX:EURUSD",
-                "width": 350,
-                "height": 220,
-                "locale": "in",
-                "dateRange": "12M",
-                "colorTheme": "light",
-                "trendLineColor": "rgba(41, 98, 255, 1)",
-                "underLineColor": "rgba(41, 98, 255, 0.3)",
-                "underLineBottomColor": "rgba(41, 98, 255, 0)",
-                "isTransparent": false,
-                "autosize": false,
-                "largeChartUrl": ""
-            });
+    // resizeAllIFramesToFitContent() {        
+    //     // or, to resize all iframes:
+    //     var iframes = document.querySelectorAll("iframe");
+    //     for( var i = 0; i < iframes.length; i++) {
+    //         this.resizeIndividualIFrameToFitContent( iframes[i] );
+    //     }        
+    // }
+
+    // resizeIndividualIFrameToFitContent( iFrame ) {
+
+    //     iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    //     iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+    // }
+
+    // renderTradingViewWidget() {
+    //     if (document.getElementsByClassName("tradingview-widget-container")) {
+    //         const script = document.createElement('script');
+    //         script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js'
+    //         script.async = true;
+    //         script.innerHTML = JSON.stringify({
+    //             "symbol": "FX:EURUSD",
+    //             "width": 350,
+    //             "height": 220,
+    //             "locale": "in",
+    //             "dateRange": "12M",
+    //             "colorTheme": "light",
+    //             "trendLineColor": "rgba(41, 98, 255, 1)",
+    //             "underLineColor": "rgba(41, 98, 255, 0.3)",
+    //             "underLineBottomColor": "rgba(41, 98, 255, 0)",
+    //             "isTransparent": false,
+    //             "autosize": false,
+    //             "largeChartUrl": ""
+    //         });
     
-            document.getElementsByClassName("tradingview-widget-container")[0].appendChild(script);
-        };
-    }
+    //         document.getElementsByClassName("tradingview-widget-container")[0].appendChild(script);
+    //     };
+    // }
 
 
-    render() {
+    render() {      
+
         return (
             <Card>
                 <Card.Body>
@@ -46,13 +62,19 @@ class StockMarketCard extends Component {
                     <div className="card-text">
 
 
-                        <div id="stock_widget_1" className="stock-widget">
+                        {/* TradingView widget */}
+                        {/* <div id="stock_widget_1" className="stock-widget">
                             <div class="tradingview-widget-container">
                                 <div class="tradingview-widget-container__widget"></div>
                                 <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/EURUSD/?exchange=FX" rel="noopener" target="_blank"><span class="blue-text">EURUSD Rates</span></a> by TradingView</div>                        
                             </div>
-                        </div>
-                    
+                        </div> */}
+
+
+                        {/* Tickertape widget  */}
+                        <iframe id="ticker_tape_card" title="tickertapeFrame" src="https://www.gateway-tt.in/trade?orderConfig=%5B%7B%22quantity%22%3A10%2C%22ticker%22%3A%22RELIANCE%22%7D%5D&cardsize=big&withSearch=true&withTT=true"
+                         width="500px" height="350px"></iframe>
+
 
                     </div>
                 </Card.Body>
